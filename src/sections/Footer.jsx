135b073a3abdd990a3457f027a0dfb6b5e531248
Pinaka <viveksharma_240087@aitpune.edu.in>
2025-06-11 21:25:27 +0530

    footer made responsive

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -10,14 +10,14 @@ import email from "../assets/Footer/email.svg"
 
 function Footer() {
   return (
-    <div className='bg-primarybg w-[95vw] h-auto flex flex-col md:flex-row gap-y-8 justify-between px-[13px] align-top pt-[1.1675rem] pb-[0.375rem] rounded-xl'>
+    <div className='bg-primarybg w-[95vw] h-auto flex flex-col md:flex-row gap-y-8 justify-between px-3.5 md:pl-9 md:pb-7 align-top pt-[1.1675rem] pb-6 rounded-xl '>
         <div className='basis-1/3 flex flex-col justify-center gap-y-4 mt-[4px] '>
             {/* icon part */}
             <div className='flex flex-row gap-2 md:justify-start justify-center align-top'>
-                <img className="w-[1.8875rem] md:w-[4.0825rem] h-auto" src={logo} alt="Gdsc" />
+                <img className="w-10 md:w-[4.25rem] h-auto" src={logo} alt="Gdsc" />
                 <div className='flex flex-col '>
                     <div>
-                        <p className='text-[0.8125rem] md:text-[1.1494rem]'>Google Developer Student Clubs</p>
+                        <p className='text-[0.8125rem]  md:text-[1.1494rem]'>Google Developer Student Clubs</p>
                     </div>
                     <div>
                         <p className='text-[0.90rem] md:text-[1.3494rem]'>Army Institute of Technology, Pune</p>
@@ -25,26 +25,26 @@ function Footer() {
                 </div>
             </div>
             {/* text part */}
-            <div className='w-full flex justify-center'>
-                <p className='text-[0.7052rem] text-center w-2/3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <div className='w-full flex justify-center md:justify-start'>
+                <p className='text-xs text-center md:text-start md:text-xl w-2/3 md:max-w-4/5 md:min-w-3/4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
             </div>
             {/* icon part */}
-            <div className='flex flex-row gap-3 justify-center md:justify-start mt-[0.9rem]'>
-                <img className='h-[0.9395rem] w-auto' src={insta} alt="instagram" />
-                <img className='h-[0.9395rem] w-auto' src={twitter} alt="twitter" />
-                <img className='h-[0.9395rem] w-auto' src={fb} alt="facebook" />
-                <img className='h-[0.9395rem] w-auto' src={linkedin} alt="linkedin" />
+            <div className='flex flex-row gap-6 md:gap-9 justify-center md:justify-start mt-4 '>
+                <img className='h-6 md:h-8 w-auto' src={insta} alt="instagram" />
+                <img className='h-6 md:h-8 w-auto' src={twitter} alt="twitter" />
+                <img className='h-6 md:h-8 w-auto' src={fb} alt="facebook" />
+                <img className='h-6 md:h-8 w-auto' src={linkedin} alt="linkedin" />
             </div>
         </div>
-        <div className='basis-2/3 flex justify-around flex-row gap-2 text-[0.3763rem]'>
+        <div className='basis-2/3 flex justify-around md:items-center flex-row gap-2 text-[0.3763rem]'>
             {/* explore */}
             <div className='flex flex-col gap-3'>
                 <div>
-                    <p className='text-[0.7237rem]' >Explore</p>
+                    <p className='text-xl md:text-[2.5rem]' >Explore</p>
                 </div>
                 <div>
-                    <ul className='list-disc list-inside space-y-1'>
-                        <li>Home</li>
+                    <ul className='list-disc list-inside space-y-1.5 md:space-y-3 text-xs md:text-xl'>
+                        <li >Home</li>
                         <li>About</li>
                         <li>Events</li>
                         <li>Team</li>
@@ -54,10 +54,10 @@ function Footer() {
             {/* quick links */}
             <div className='flex flex-col gap-3'>
                 <div>
-                    <p className='text-[0.7237rem]'>Quick Links</p>
+                    <p className='text-xl md:text-[2.5rem]'>Quick Links</p>
                 </div>
                 <div>
-                    <ul className='list-disc list-inside space-y-1'>
+                    <ul className='list-disc list-inside space-y-1.5 md:space-y-3 text-xs md:text-xl'>
                         <li>Privacy</li>
                         <li>Terms & Conditions</li>
                         <li>Upcoming Events</li>
@@ -68,18 +68,18 @@ function Footer() {
             {/* get in touch (appropriately 😁) */}
             <div className='flex flex-col gap-3'>
                 <div>
-                    <p className='text-[0.7237rem] '>Get in Touch</p>
+                    <p className='text-xl md:text-[2.5rem]'>Get in Touch</p>
                 </div>
-                <div className='flex flex-col gap-[0.405rem]'>
-                    <div className='flex flex-row gap-1 justify-start items-center'> 
+                <div className='flex flex-col gap-2 md:gap-6 '>
+                    <div className='flex flex-row gap-1 justify-start items-center text-xs md:text-xl'> 
                         <img src={address} alt="" />
                         <p>Home</p>
                     </div>
-                    <div className='flex flex-row gap-1 justify-start items-center'>
+                    <div className='flex flex-row gap-1 justify-start items-center text-xs md:text-xl'>
                         <img src={mobile} alt="" />
                         <p>Mob NO</p>
                     </div>
-                    <div className='flex flex-row gap-1 justify-start items-center'>
+                    <div className='flex flex-row gap-1 justify-start items-center text-xs md:text-xl'>
                         <img src={email} alt="" />
                         <p>Email</p>
                     </div>

607e68675803a621c96bc7522ca29006d8afae0e
Pinaka <viveksharma_240087@aitpune.edu.in>
2025-06-12 18:50:44 +0530

    Event Section made responsive

--- src/components/Primarybtn.jsx
+++ src/components/Primarybtn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-function Primarybtn() {
+function Primarybtn({label}) {
 
     
     
@@ -9,8 +9,8 @@ function Primarybtn() {
   return (
     <div className='flex justify-center items-center rounded-full w-fit relative h-fit
      bg-white border-[0.0513rem] border-black border-solid'>
-        <div className='w-[60%] mr-4 ml-1 h-auto flex justify-start items-center'>
-            <p className='text-[0.5rem] py-1.5 pr-5 text-center font-bold  translate-y-[0.5px]'>FLUTTER STUDY JAMS</p>
+        <div className='w-[60%]  mr-4 ml-1 h-auto flex justify-start items-center'>
+            <p className='text-[0.5rem] py-1.5 pr-5 text-center font-bold md:text-[0.7rem]  translate-y-[0.5px]'>{label}</p>
         </div>
         
         <div className='absolute flex border-l-[0.0513rem] border-black border-solid  justify-center items-center right-0 top-1/2 transform -translate-y-[50%] rounded-full h-full aspect-square   bg-blue-300 z-5 '>
--- src/components/cards/EventCard.jsx
+++ src/components/cards/EventCard.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+// import React from 'react'
 
+// import Primarybtn from '../Primarybtn'
+
+// function EventCard({eventimage, eventlogo}) {
+//   return (
+//     <div className='flex flex-row gap-2 justify-center items-center w-full max-w-[450px] border-solid py-4 px-3.5 border-black border-[0.0625rem] bg-transparent rounded-xl'>
+//         <div className='flex flex-col w-full gap-y-5 justify-center items-center'>
+//             <div>
+//                 <img className='w-14 md:w-20 h-auto' src={eventlogo} alt="hello" />
+//             </div>
+//             <div>
+//                 <Primarybtn/>
+//             </div>
+//         </div>
+//         <div className='flex justify-center w-full items-center'>
+//             <img className='h-auto w-full max-w-[250px] md:max-w-[250px] lg:max-w-[250px] rounded-xl' src={eventimage} alt="" />
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default EventCard
+
+import React from 'react'
 import Primarybtn from '../Primarybtn'
 
-function EventCard({eventimage, eventlogo}) {
+function EventCard({ eventimage, eventlogo }) {
   return (
-    <div className='flex flex-row gap-2 justify-center items-center w-full max-w- border-solid py-4 px-3.5 border-black border-[0.0625rem] bg-transparent rounded-xl'>
-        <div className='flex flex-col w-full gap-y-5 justify-center items-center'>
-            <div>
-                <img className='w-14 h-auto' src={eventlogo} alt="hello" />
-            </div>
-            <div>
-                <Primarybtn/>
-            </div>
-        </div>
-        <div className='flex justify-center w-full items-center'>
-            <img className='h-auto w-full max-w-[250px] md:max-w-[250px] lg:max-w-[250px]  rounded-xl' src={eventimage} alt="" />
-        </div>
+    <div className="flex flex-row gap-2 justify-center items-center border border-black py-4 px-3.5 bg-transparent rounded-xl w-[90vw] sm:w-[400px] md:w-[450px]">
+      <div className="flex flex-col gap-y-5 justify-center items-center flex-shrink-0 w-[40%]">
+        <img className="w-14 md:w-20 h-auto" src={eventlogo} alt="logo" />
+        <Primarybtn label="FLUTTER STUDY JAMS"/>
+      </div>
+
+      <div className="flex justify-center items-center w-[60%]">
+        <img
+          className="h-auto w-full max-w-[250px] rounded-xl"
+          src={eventimage}
+          alt="event"
+        />
+      </div>
     </div>
   )
 }

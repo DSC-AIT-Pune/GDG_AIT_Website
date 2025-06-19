bebb5b481623ff026f06d7efe0c51f3c2f9dd97b
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-06-19 11:35:38 +0530

    Add About Us section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Footer from './sections/Footer'
-import Primarybtn from './components/Primarybtn'
-import EventCard from './components/cards/EventCard'
 import Events from './sections/Events'
-import IntroCard from './components/cards/IntroCard'
-import TeamBar from './components/TeamBar'
 import Team from './sections/Team'
-import TeamCard from '../../oldinstpi/Gdsc_website/src/components/TeamCard'
+import Home from './sections/Home'
+import About from './sections/About'
 
 function App() {
   return (
     <>
+
+      <Home/>
+      <About/>
       <Team/>
+      <Events/>
+      <Footer/>
     </>
   )
 }
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -9,12 +9,12 @@ import join from '../Assets/Home/join.png';
 
 const Home = () => {
   return (
-    <section className="relative min-h-screen bg-[#E6E4D5] text-black" class="svg-background">
+    <section className="relative w-[95vw] rounded-2xl min-h-screen bg-[#E6E4D5] text-black" class="svg-background">
       {/* Navbar */}
       <div className="flex p-0 justify-between items-center w-[100%]">
         {/* Logo Section with Beige Background */}
         <div className="bg-beige py-4 w-[20%] flex justify-center items-center pr-28">
-          <div className="w-12 h-10">
+          <div className="w-10 h-8">
             <svg width="113" height="62" viewBox="0 0 113 62" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M81.3573 33.3811C87.3027 29.567 93.2321 25.7217 99.1977 21.947C104.86 18.3636 111.522 21.6684 112.806 28.642C113.549 32.6761 111.936 37.0341 108.676 39.1566C97.9856 46.1149 87.2801 53.0456 76.5213 59.8702C73.1913 61.9824 69.7612 61.5939 66.7265 59.0054C63.6437 56.3775 62.6722 52.7527 63.3952 48.5337C64.2206 45.3221 66.1099 43.1749 68.6863 41.5685C72.9464 38.913 77.1371 36.1175 81.3573 33.3811Z" fill="#FABC05"/>
             <path d="M85.7228 30.6073C79.7925 26.7636 73.8472 22.9499 67.9376 19.0672C62.3278 15.3812 61.5598 7.28451 66.3243 2.55526C69.0804 -0.179634 73.2654 -0.799386 76.5415 1.29357C87.2818 8.15522 98.008 15.046 108.679 22.0407C111.981 24.2057 113.395 27.7186 112.906 31.9484C112.409 36.2437 110.085 38.9959 106.453 40.4065C103.549 41.2141 100.94 40.4597 98.4066 38.7706C94.2172 35.9775 89.9545 33.321 85.7228 30.6073Z" fill="#109D58"/>
@@ -54,10 +54,10 @@ const Home = () => {
           {/* First Div: GDSC Letters */}
           <div className=" flex space-x-1 ">
             <div>
-            <span className="text-9xl  text-red-600">G</span>
-            <span className="text-9xl  text-blue-500">D</span>
-            <span className="text-9xl  text-green-600">S</span>
-            <span className="text-9xl  text-yellow-500">C</span>
+            <span className="text-9xl  text-red-600 font-extrabold">G</span>
+            <span className="text-9xl  text-blue-500 font-extrabold">D</span>
+            <span className="text-9xl  text-green-600 font-extrabold">S</span>
+            <span className="text-9xl  text-yellow-500 font-extrabold">C</span>
             </div>
             <div className='pl-12 pt-12'>
             <span> <svg xmlns="http://www.w3.org/2000/svg" width="370" height="16" viewBox="0 0 243 16" fill="none">
@@ -66,8 +66,8 @@ const Home = () => {
             </div>
           </div>
           <div className='space-x-8 pt-0'>
-            <span className="text-9xl text-black" >AIT</span>
-            <span className="text-9xl text-black" >PUNE</span>
+            <span className="text-9xl text-black font-extrabold" >AIT</span>
+            <span className="text-9xl text-black font-extrabold" >PUNE</span>
           </div>
         </div>
         <div className='-mt-3 ml-20'>

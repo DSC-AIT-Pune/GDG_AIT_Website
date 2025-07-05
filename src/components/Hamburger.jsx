f906d59b78000f8758b1666becde70664e8b169a
pinak <viveksharma_240087@aitpune.edu.in>
2025-07-05 23:08:17 +0530

    sidebar fixed

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -13,8 +13,8 @@ function Hamburger({ isOpen, onClose }) {
   ];
 
   return (
-    <div className={`fixed top-0 left-0 h-full w-72 bg-black z-50 transform transition-transform duration-300 ease-in-out ${
-        isOpen ? 'translate-x-0' : '-translate-x-[120%]'
+    <div className={`fixed top-0 left-0 h-full bg-black z-50 transform transition-transform duration-300 ease-in-out ${
+        isOpen ? ' translate-x-0' : '-translate-x-full '
       }`}>
       <div className="flex flex-col  bg-black gap-y-3 h-screen w-[86vw]">
         <div className="flex flex-row px-2 pt-7 justify-between">

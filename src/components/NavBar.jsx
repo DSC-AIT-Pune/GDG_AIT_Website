18045ac2264f04c090eca956781eac311ac81390
pinak <viveksharma_240087@aitpune.edu.in>
2025-07-05 23:54:36 +0530

    desktop navbar functionality added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,17 +3,29 @@ import './NavBar.css'
 import navbg from '../assets/Home/navbg.svg'
 
 function NavBar() {
+  const menuitem = ['Home', 'About Us', 'Events', 'Team', 'Calender', 'Contact Us'];
   return (
     <div className='relative w-fit px-16 mt-0 pt-0 pb-4'>
         <img className='absolute top-0 left-1/2 -z-10 transform -translate-x-1/2' src={navbg} alt="" />
             <ul>
                 <div className='flex flex-row m-0 p-2 gap-9 leading-none text-[clamp(0.5rem,1.5vw,1.2rem)] text-nowrap text-white  '>
-                    <li >Home</li>
-                    <li>About Us</li>
-                    <li>Events</li>
-                    <li>Team</li>
-                    <li>Calender</li>
-                    <li>Contact Us</li>
+                    {menuitem.map((item, index) => {
+                        const sectionId = item.toLowerCase();
+                        return (
+                            <li
+                                key={index}
+                                className='cursor-pointer hover:text-[#a1a095] transition-colors duration-300'
+                                onClick={() => {
+                                    const section = document.getElementById(sectionId);
+                                    if (section) {
+                                        section.scrollIntoView({ behavior: 'smooth' });
+                                    }
+                                }}
+                            >
+                                {item}
+                            </li>
+                        );
+                    })}
                     
                 </div>
             </ul>
